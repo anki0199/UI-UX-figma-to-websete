3c6af3c94da098f47f6ca48f156956c6c93617d5
Ankita <ankum1999@gmail.com>
2023-03-14 17:54:47 +0530

    Update Footer.jsx

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
       </div>
 
       <div className='gpt3__footer-links'>
-        <div className='gpt3__footer-links-logo'>
+        <div className='gpt3__footer-links_logo'>
           <img src={gpt3logo} alt="logo"/>
           <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
         </div>
